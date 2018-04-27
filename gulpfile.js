5da0dcac49085564460da176244f77fab6c47431
petromath77 <petromath77@gmail.com>
2018-04-27 17:37:17 +0300

    slider&nav

--- gulpfile.js
+++ gulpfile.js
@@ -35,6 +35,7 @@ gulp.task('sass', function() {
 gulp.task('js', function() {
 	return gulp.src([
 		'app/libs/jquery/dist/jquery.min.js',
+		'app/libs/bootstrap/js/bootstrap.min.js',
 		'app/js/common.js', // Always at the end
 		])
 	.pipe(concat('scripts.min.js'))
